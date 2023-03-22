158b17fd67b27e4f5232f77676097e244f163a75
VijayKumar4003 <38779300+VijayKumar4003@users.noreply.github.com>
2023-03-22 12:08:35 +0530

    Pratise Function and Variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import First from './components/First'
 function App() {
-  const [count, setCount] = useState(0)
+  
+  let myName = "Slow Bro Coder"
+ 
+  function myfunction(){
+
+      myName = 'slow_bro_coder'
+      alert(myName)
+  }
 
   return (
-    <div className="App">
-      <div>
+    <>
+        
+        <h1>This is where World will Change</h1>
+        <button onClick={()=>myfunction()}>Click the Button</button>
 
-
-      <h2>This is the Project that we want</h2>
-      <h2>Now it has the branch code for the development</h2>
-
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+        <h2>{myName}</h2>
+    </>
   )
 }
 
