d8666b62c56b59f771d4f7c9b6b8ec85446fc432
VijayKumar4003 <38779300+VijayKumar4003@users.noreply.github.com>
2023-03-23 21:47:49 +0530

    msg

--- src/components/LearnSates.jsx
+++ src/components/LearnSates.jsx
@@ -1,32 +1,18 @@
 import {useState} from 'react'
 
 export const LearnSates = () => {
-  
  const [name,setDataName] = useState('Vijay Name will be Updated')
- 
- 
   function updateNameData(){
-
     setDataName('Vijay Kumar')
-
-   
-   
     // alert(name)
-
   }
-
 
   return (
     <div>
-
       <h1 style={{color:'red'}}>Learn States in React</h1>
       <hr />
-      
       <h2>{name}</h2>
       <button onClick={()=>updateNameData()}>Click Button</button>
-
     </div>
   )
-
-
 }
